466e78e689028066ef765fa4d21deab45c9217b9
Tyler Acosta <tacos1998@gmail.com>
2015-11-10 23:24:38 +0000

    General improvements

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://www.heartinternet.uk/assets/images/cache/assets/images/powerup/icon-user-blog_100x100.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
